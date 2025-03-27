be92dd172f2289349b739d4b630826e424444c81
Marco Braga <maboa17@gmail.com>
2025-03-27 12:39:40 -0300

    add login redirection link to SelectAccountType and update ForgotPassword API endpoint

--- src/pages/SelectAccountType.tsx
+++ src/pages/SelectAccountType.tsx
@@ -32,6 +32,14 @@ export default function SelectAccountType() {
             Register as Consumer
           </button>
         </div>
+        <div>
+          <p className="mt-4 text-sm font-light text-gray-500 dark:text-gray-400">
+            Already have an account?{' '}
+            <a href="/login" className="font-medium text-blue-600 hover:underline dark:text-blue-500">
+              Sign in
+            </a>
+          </p>
+        </div>
       </div>
       <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
         <img
