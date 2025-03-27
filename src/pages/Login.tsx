8ae23fc5e5ef602927cbe67f621cbd87d9274ff5
Marco Braga <maboa17@gmail.com>
2025-03-27 00:03:53 -0300

    add new attribute input tag

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -76,6 +76,7 @@ export default function Login() {
                 <input
                   type="email"
                   id="email"
+                  autoComplete="email"
                   placeholder="name@company.com"
                   {...register('email')}
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -92,6 +93,7 @@ export default function Login() {
                 <input
                   type="password"
                   id="password"
+                  autoComplete="current-password"
                   placeholder="••••••••"
                   {...register('password')}
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
